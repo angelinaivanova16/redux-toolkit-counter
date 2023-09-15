5feba480c349d9a56be235d3379f184014838f90
Angelina <angelinaivanova16@gmail.com>
2023-09-15 13:22:23 +0300

    add layout and styles

--- src/App.js
+++ src/App.js
@@ -1,23 +1,12 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='app'>
+      <h1>Счетчик</h1>
+      <p className='result'>0</p>
+      <button className='btn'>+</button>
+      <button className='btn'>-</button>
     </div>
   );
 }
