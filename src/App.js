07c7eb56c5db459ccb40aa3e51e449239caa31f1
Angelina <angelinaivanova16@gmail.com>
2023-09-19 10:22:40 +0300

    connect store with UI, create slice, add slice to store, subscribe to state and changes

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import './App.css';
+import { useSelector } from 'react-redux';
+import { useDispatch } from 'react-redux';
+import { plus, minus } from './redux/counter-reducer';
 
 function App() {
+  const count = useSelector(state => state.counter.value);
+  const dispatch = useDispatch();
   return (
     <div className='app'>
       <h1>Счетчик</h1>
-      <p className='result'>0</p>
-      <button className='btn'>+</button>
-      <button className='btn'>-</button>
+      <p className='result'>{count}</p>
+      <button className='btn' onClick={() => dispatch(plus())}>+</button>
+      <button className='btn' onClick={() => dispatch(minus())}>-</button>
     </div>
   );
 }
